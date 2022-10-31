d023c67cf9f1f89ab1c99ccb3f4a5563a4c5a543
Alexander Flink <hi@alexanderflink.com>
2022-10-31 23:04:53 +0100

    add telescope keymap

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -1,11 +1,21 @@
 require("telescope").setup({
-	extensions = {
-		file_browser = {
-			hijack_netrw = true,
-		},
-	},
+	pickers = {
+		"buffers",
+		"find_files",
+		"grep_string",
+		"live_grep",
+		"marks",
+		"keymaps",
+		"diagnostics",
+		"lsp_workspace_symbols",
+		"lsp_document_symbols",
+		"commands",
+		"quickfix",
+		"registers"
+	}
 })
 
 require("telescope").load_extension("file_browser")
+vim.keymap.set("n", "<leader>t", ":Telescope<CR>", opts)
 vim.keymap.set("n", "<leader>bf", ":Telescope buffers<CR>", opts)
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", opts)
