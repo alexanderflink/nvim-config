5115dd3c706f1d27f07266f64a7c836e02aa5738
Alexander Flink <hi@alexanderflink.com>
2022-11-01 10:41:02 +0100

    ensure_installed treesitter

--- plugin/nvim-treesitter.lua
+++ plugin/nvim-treesitter.lua
@@ -1,4 +1,7 @@
 require("nvim-treesitter.configs").setup({
+	ensure_installed = {
+		"css", "dockerfile", "fish", "git_rebase", "gitattributes", "gitignore", "glsl", "go", "graphql", "html", "http", "javascript", "json", "lua", "php", "python", "rust", "scss", "sql", "tsx", "typescript", "yaml"
+	},
 	highlight = {
 		enable = true,
 	},
