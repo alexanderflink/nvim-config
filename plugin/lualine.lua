6fd6b65251cb459915a98d08dea7b8cc00b8da5e
Alexander Flink <hi@alexanderflink.com>
2022-11-03 22:08:32 +0100

    add config

--- plugin/lualine.lua
+++ plugin/lualine.lua
@@ -1,10 +1,13 @@
 require("lualine").setup({
+	options = {
+		globalstatus = true,
+	},
 	sections = {
 		lualine_c = {
 			{
 				"filename",
 				file_status = true,
-				path = 1,
+				path = 3,
 			},
 		},
 	},
--- plugin/nvim-tree.lua
+++ plugin/nvim-tree.lua
@@ -1,5 +1,6 @@
 require("nvim-tree").setup({
 	sync_root_with_cwd = true,
+	respect_buf_cwd = true,
 	view = {
 		adaptive_size = true,
 	},
