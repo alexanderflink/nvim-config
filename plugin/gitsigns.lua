a695daf8d8442805d92aeadc17093812dfa77871
Alexander Flink <hi@alexanderflink.com>
2022-11-04 22:22:33 +0100

    add keymap

--- plugin/gitsigns.lua
+++ plugin/gitsigns.lua
@@ -1,11 +1,16 @@
 require("gitsigns").setup({
 	on_attach = function(bufnr)
-		print("gitsigns on_attach")
 		vim.keymap.set(
 			"n",
 			"<leader>ghr",
 			":Gitsigns reset_hunk<CR>",
 			{ noremap = true, silent = true, buffer = bufnr }
 		)
+
+		vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", {
+			noremap = true,
+			silent = true,
+			buffer = bufnr,
+		})
 	end,
 })
