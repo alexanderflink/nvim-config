d673d9bfba56c1c44a888d39cde4ab0ffc24ec0c
Alexander Flink <hi@alexanderflink.com>
2022-11-04 21:48:48 +0100

    add vim-sleuth and config

--- plugin/nvim-treesitter.lua
+++ plugin/nvim-treesitter.lua
@@ -1,14 +1,35 @@
 require("nvim-treesitter.configs").setup({
 	ensure_installed = {
-		"css", "dockerfile", "fish", "git_rebase", "gitattributes", "gitignore", "glsl", "go", "graphql", "html", "http", "javascript", "json", "lua", "php", "python", "rust", "scss", "sql", "tsx", "typescript", "yaml"
+		"css",
+		"dockerfile",
+		"fish",
+		"git_rebase",
+		"gitattributes",
+		"gitignore",
+		"glsl",
+		"go",
+		"graphql",
+		"html",
+		"http",
+		"javascript",
+		"json",
+		"lua",
+		"php",
+		"python",
+		"rust",
+		"scss",
+		"sql",
+		"tsx",
+		"typescript",
+		"yaml",
 	},
 	highlight = {
 		enable = true,
 	},
 	autotag = {
-		enable = true
+		enable = true,
 	},
 	indent = {
-		-- enable = true
-	}
+		enable = true,
+	},
 })
