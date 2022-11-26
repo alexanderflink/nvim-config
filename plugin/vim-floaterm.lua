4b0e76d29a8cfcd9cf95de37fe4ce1ca87a79d7e
Alexander Flink <hi@alexanderflink.com>
2022-11-26 11:30:10 +0100

    remove floaterm

--- plugin/vim-floaterm.lua
+++
@@ -1,7 +0,0 @@
-local opts = { noremap = true, silent = true }
-
-vim.keymap.set("n", "<leader>tt", ":FloatermToggle<CR>", opts)
-vim.keymap.set("n", "<leader>tp", ":FloatermPrev<CR>", opts)
-vim.keymap.set("n", "<leader>tn", ":FloatermNext<CR>", opts)
-vim.keymap.set("n", "<leader>tk", ":FloatermKill<CR>", opts)
-vim.keymap.set("n", "<Esc>", ":FloatermHide<CR>", { remap = true, silent = true })
